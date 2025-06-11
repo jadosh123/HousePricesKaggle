d7a5eacb155478e67970cee42ba1e917935e7ad8
jad <jadosh2000@gmail.com>
2025-06-11 15:43:52 +0300

    Changes to utils.py now provide column names of >15% missing data, refractored the custom transformers and added new feature, Achieves rmsle of 0.13291, new high score

--- src/utils.py
+++ src/utils.py
@@ -2,25 +2,38 @@ import pandas as pd
 
 
 # Drops majority null features
-def drop_null_majority_features(df_train: pd.DataFrame, df_test: pd.DataFrame):
-    cols_to_drop = []
+def drop_null_majority_features(df_train: pd.DataFrame, df_test: pd.DataFrame, target_col='SalePrice'):
+    features_to_drop = []
+    features_created = []
+
     for col in df_train.columns:
-        if df_train[col].isnull().sum() > len(df_train[col])/2:
-            cols_to_drop.append(col)
+        if col == target_col:
+            continue
+        missing_pct = df_train[col].isnull().sum() / len(df_train)
 
-    df_train.drop(cols_to_drop, axis=1, inplace=True)
-    df_test.drop(cols_to_drop, axis=1, inplace=True)
+        if missing_pct > 0.75:
+            features_to_drop.append(col)
+        elif missing_pct > 0.15:
+            features_created.append(col)
 
-
-# Drops features with single value majority (above 80%)
-def drop_single_val_majority(df_train: pd.DataFrame, df_test: pd.DataFrame):
-    cols_to_drop = []
+    print("Analyzing single-value dominant features:")
     for col in df_train.columns:
-        var_count = df_train[col].value_counts() / df_train.shape[0]
-        for key in var_count.keys():
-            if var_count[key] > 0.80:
-                cols_to_drop.append(col)
+        if col == target_col or col in features_to_drop:
+            continue
 
-    # print(cols_to_drop)
-    df_train.drop(cols_to_drop, axis=1, inplace=True)
-    df_test.drop(cols_to_drop, axis=1, inplace=True)
+        if df_train[col].dtype in ['object', 'category']:
+            # For categorical
+            value_counts = df_train[col].value_counts(normalize=True, dropna=False)
+            if len(value_counts) > 0 and value_counts.iloc[0] > 0.95:
+                features_to_drop.append(col)
+                print(f"  DROPPING {col}: {value_counts.iloc[0]*100:.1f}% are '{value_counts.index[0]}'")
+        else:
+            # For numeric
+            if df_train[col].nunique() == 1:  # Truly constant
+                features_to_drop.append(col)
+                print(f"  DROPPING {col}: Constant value")
+
+    df_train.drop(features_to_drop, axis=1, inplace=True)
+    df_test.drop(features_to_drop, axis=1, inplace=True)
+
+    return features_created
